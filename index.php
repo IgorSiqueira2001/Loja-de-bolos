3e9389755d5ee8a0810c21f3f6fd5c2101cc7a66
Igor Antonio <igorassi2001@gmail.com>
2025-03-01 17:14:07 -0300

    Edicao do visual

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Loja de Bolos</title>
+</head>
+<body>
+    <form action="index.php" method="POST">            
+        <div class="parent">
+                <div class="div5">
+                    <h3 style="display: flex;
+                                flex-flow: row wrap;
+                                align-items: center;
+                                justify-content: center;">Bolo de Chocolate</h3>
+                </div>
+            <div class="div6">
+                <h3 style="display: flex;
+                            flex-flow: row wrap;
+                            align-items: center;
+                            justify-content: center;">Bolo de Morango</h3>
+            </div>
+            <div class="div7">
+                <h3 style="display: flex;
+                            flex-flow: row wrap;
+                            align-items: center;
+                            justify-content: center;">Bolo de Abacaxi</h3>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
